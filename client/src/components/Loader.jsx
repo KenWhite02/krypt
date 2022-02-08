66d3bc21c68388e5468c200511db1b9ce0298fba
Ken White <whiteken2002@gmail.com>
2022-02-08 10:59:51 +0300

    Modified loading spnner

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,5 +1,9 @@
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="flex justify-center items-center y-3">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-red-700" />
+    </div>
+  );
 };
 
 export default Loader;
