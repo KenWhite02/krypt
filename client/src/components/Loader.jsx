6548e498a6d1d3f474e9e44843d0097634e61be1
Ken White <whiteken2002@gmail.com>
2022-02-10 16:27:50 +0300

    Removed client folder

--- client/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex justify-center items-center y-3">
-      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-red-700" />
-    </div>
-  );
-};
-
-export default Loader;
