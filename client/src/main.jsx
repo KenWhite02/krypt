aa99c5ee2fc78bc3c7e960dca82fe0273a68158c
Ken White <whiteken2002@gmail.com>
2022-02-10 16:10:21 +0300

    Wrapped application with context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { TransactionProvider } from './context/TransationContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TransactionProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionProvider>,
   document.getElementById('root')
-)
+);
